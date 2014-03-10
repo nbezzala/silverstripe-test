8d8c1c51950ca6b867535b211df3d41473f2f151
Nitish Bezzala <nbezzala@yahoo.com>
2014-03-09 23:05:45 -0500

    New controller to display browser poll form on home page.

---
+++ mysite/code/HomePage.php
@@ -0,0 +1,35 @@
+<?php
+class HomePage extends Page {
+
+}
+
+class HomePage_Controller extends Page_Controller {
+    private static $allowed_actions = array('BrowserPollForm');
+
+    public function BrowserPollForm() {
+        $fields = new FieldList(
+            new TextField('Name', 'Your Name'),
+            new TextField('Email', 'Your Email'),
+            new OptionsetField('Browser', 'Your Favourite Browser', array(
+                'Internet Explorer' =>  'Internet Explorer',
+                'Firefox'           =>  'Firefox',
+                'Chrome'            =>  'Chrome',
+                'Safari'            =>  'Safari',
+                'Konqueror'         =>  'Konqueror',
+                'Opera'             =>  'Opera',
+                'Lynx'              =>  'Lynx',
+            )),
+            new TextAreaField('Reason')
+        );
+
+        $actions = new FieldList(
+            new FormAction('doBrowserPoll', 'Submit')
+        );
+        
+        return new Form($this, 'BrowserPollForm', $fields, $actions);
+    }
+	
+    public function init() {
+		parent::init();
+    }
+}
